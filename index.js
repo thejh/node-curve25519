dd7985b36fa594de4ac8f442844089ec4a0466bb
Jann Horn <jannhorn@googlemail.com>
2011-06-24 21:39:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var binding = require("./build/default/curve");
+
+var basepoint = (function() {
+  var buf = new Buffer(32);
+  buf[0] = 9;
+  for (var i=1; i<32; i++) {
+    buf[i] = 0;
+  }
+  return buf;
+})();
+
+exports.makeSecretKey = function(mysecret) {
+  if (!mysecret instanceof Buffer)
+    throw 'mysecret must be a Buffer';
+  if (mysecret.length != 32)
+    throw 'mysecret must be 32 bytes long';
+  mysecret[0] &= 248;
+  mysecret[31] &= 127;
+  mysecret[31] |= 64;
+}
+
+exports.derivePublicKey = function (mysecret) {
+  var mypublic = new Buffer(32);
+  binding.curve(mypublic, mysecret, basepoint);
+  return mypublic;
+}
+
+exports.deriveSharedSecret(mysecret, hispublic) {
+  var sharedSecret = new Buffer(32);
+  binding.curve(sharedSecret, mysecret, hispublic);
+  return sharedSecret;
+}
